84ea7851cf1660e2486351890a1b856d2cda77b0
Hongyuan Liu <liustev6@hongyuan.home>
2023-05-28 02:32:26 -0400

    feat: add simple API requsts

--- lib/helper/http_helper.dart
+++ lib/helper/http_helper.dart
@@ -5,7 +5,7 @@ class HttpHelper{
 
   postWeather(String location) async  {
     Map<String, dynamic> parameters = {'q': location};
-    Uri uri = Uri.parse("http://192.168.2.13:52309/mobapp");
-    http.post(uri); //can only use await in async
+    Uri uri = Uri.parse("http://192.168.2.13:52309/password?uid=1236666&password=12345");
+    http.get(uri); //can only use await in async steven is a stupid pig. lhyyw
   }
 }
